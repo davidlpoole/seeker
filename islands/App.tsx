64fa6d8481b72618c851245f9dfb85f985933a2d
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-12-17 18:24:44 +1300

    Save and load from localStorage

--- islands/App.tsx
+++ islands/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "preact/hooks";
+import { useEffect, useState } from "preact/hooks";
 
 import AddToList from "../islands/AddToList.tsx";
 import CountList from "../islands/CountList.tsx";
@@ -7,16 +7,28 @@ import Search from "../types/Search.ts";
 export default function App() {
   const [searchList, setSearchList] = useState([] as Search[]);
 
+  useEffect(() => {
+    const getSeekerList = localStorage.getItem("seekerList");
+    if (getSeekerList) {
+      setSearchList(JSON.parse(getSeekerList));
+    }
+  }, []);
+
   function addTerm(newSearch: Search) {
-    setSearchList([...searchList, newSearch]);
+    const newList = [...searchList, newSearch];
+    setSearchList(newList);
+    localStorage.setItem(
+      "seekerList",
+      JSON.stringify(newList),
+    );
   }
 
   return (
     <>
       <h1 class="text-4xl font-bold pb-4 text-white">The Seeker</h1>
       <p class="text-white mb-2">
-        Find out how many jobs are available on Seek NZ for a specific search
-        term and location (optional).
+        Find out how many jobs are available on Seek for a specific search term
+        and location (optional).
       </p>
       <AddToList addTerm={addTerm} />
       <CountList searchList={searchList} />
