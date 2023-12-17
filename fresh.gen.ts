a598162e229507c5dffcd46ea0983809f3a2dda3
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-12-17 16:53:58 +1300

    Add API endpoint to filter by location

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,7 +4,8 @@
 
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
-import * as $api_v1_scrape_searchTerm_ from "./routes/api/v1/scrape/[searchTerm].tsx";
+import * as $api_v1_scrape_searchTerm_location_index from "./routes/api/v1/scrape/[searchTerm]/[location]/index.tsx";
+import * as $api_v1_scrape_searchTerm_index from "./routes/api/v1/scrape/[searchTerm]/index.tsx";
 import * as $index from "./routes/index.tsx";
 import * as $AddToList from "./islands/AddToList.tsx";
 import * as $App from "./islands/App.tsx";
@@ -16,7 +17,10 @@ const manifest = {
   routes: {
     "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
-    "./routes/api/v1/scrape/[searchTerm].tsx": $api_v1_scrape_searchTerm_,
+    "./routes/api/v1/scrape/[searchTerm]/[location]/index.tsx":
+      $api_v1_scrape_searchTerm_location_index,
+    "./routes/api/v1/scrape/[searchTerm]/index.tsx":
+      $api_v1_scrape_searchTerm_index,
     "./routes/index.tsx": $index,
   },
   islands: {
